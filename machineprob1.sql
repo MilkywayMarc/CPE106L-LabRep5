9f79ef7914a62ee852bd8512a3b4ac76804aa315
MilkywayMarc <113160733+MilkywayMarc@users.noreply.github.com>
2022-09-30 00:54:15 +0800

    Add files via upload

---
+++ machineprob1.sql
@@ -0,0 +1,10 @@
+--
+-- File generated with SQLiteStudio v3.3.3 on Fri Sep 30 00:48:25 2022
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
