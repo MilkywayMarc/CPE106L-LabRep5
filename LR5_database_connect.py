7b9960c5a0e4008bc93e51be4d3702fb2a96fb75
MilkywayMarc <113160733+MilkywayMarc@users.noreply.github.com>
2022-09-30 22:45:11 +0800

    Add files via upload

---
+++ LR5_database_connect.py
@@ -0,0 +1,14 @@
+import sqlite3 as sql
+
+db = sql.connect("LR5.db")
+cursor = db.cursor()
+
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+print(cursor.fetchall())
+
+print("\nFetching \"ADVENTURE_TRIP\" from database:\n\n")
+cursor.execute("SELECT * FROM ADVENTURE_TRIP")
+for row in cursor:
+    print(row)
+    print("\n")
+    
